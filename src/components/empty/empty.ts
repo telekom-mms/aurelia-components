5616cfd112f068c45089542caab3716b8cebd011
mkri <mkri@t-systems-mms.com>
2019-02-13 15:25:59 +0100

    Added some common components

---
+++ src/components/empty/empty.ts
@@ -0,0 +1,10 @@
+/**
+ * Dont look at me, I'm just empty
+ * Useful for empty default routes:
+ * {
+ *  route: '',
+ *  moduleId: PLATFORM.moduleName('empty/empty'),
+ * }
+ * @author Mike Reiche <mike.reiche@t-systems.com>
+ */
+export class Empty {}
---
+++ src/components/progress-ring/progress-ring.ts
@@ -0,0 +1,47 @@
+import {bindable, autoinject} from 'aurelia-framework';
+
+/**
+ * Implementation of ProgressRing
+ * @author Mike Reiche <mike.reiche@t-systems.com>
+ * Usage: <progress-ring stroke="2" radius="20" color="green" progress.bind="progress"></progress-ring>
+ * @see https://css-tricks.com/building-progress-ring-quickly/
+ */
+@autoinject()
+export class ProgressRing {
+    private _circumference:number=0;
+    private _normalizedRadius:number=0;
+
+    @bindable color:string='white';
+    @bindable stroke:number=2;
+    @bindable radius:number=40;
+    @bindable progress:number=0;
+
+    constructor(
+        private _element:Element
+    ) {
+    }
+
+    bind() {
+        this.updateSize();
+        this.progressChanged(0);
+    }
+
+    private updateSize() {
+        this._normalizedRadius = this.radius - this.stroke * 2;
+        this._circumference = this._normalizedRadius * 2 * Math.PI;
+    }
+
+    radiusChanged(radius:number) {
+        this.updateSize();
+    }
+
+    private strokeChanged(stroke:number) {
+        this.updateSize();
+    }
+
+    progressChanged(percent:number) {
+        const offset = this._circumference - (percent / 100 * this._circumference);
+        const circle = this._element.querySelector('circle');
+        circle.style.strokeDashoffset = offset.toString();
+    }
+}
