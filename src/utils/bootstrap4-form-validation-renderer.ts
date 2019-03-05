769b35e34174afa93fd870abeced52f2b8a10039
Mike Reiche <mike.reiche@t-systems.com>
2019-03-05 17:38:15 +0100

    Added inline doc

--- src/utils/bootstrap4-form-validation-renderer.ts
+++ src/utils/bootstrap4-form-validation-renderer.ts
@@ -6,6 +6,11 @@ import {
     ValidationRules
 } from "aurelia-validation";
 
+/**
+ * Form validation renderer for bootstrap 4
+ * @see https://aurelia.io/docs/plugins/validation#custom-renderers
+ * @author Mike Reiche <mike.reiche@t-systems.com>
+ */
 export class Bootstrap4FormValidationRenderer {
     render(instruction: RenderInstruction) {
         for (let { result, elements } of instruction.unrender) {
