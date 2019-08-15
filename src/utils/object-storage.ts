2b995c501c7bd828fb15b454a18d5b7615a2f098
Mike Reiche <mike.reiche@t-systems.com>
2019-08-15 13:25:13 +0200

    Added ObjectStorage

---
+++ src/utils/object-storage.ts
@@ -0,0 +1,21 @@
+/**
+ * Allows storage of objects into storage interface
+ * @author Mike Reiche <mike.reiche@t-systems.com>
+ */
+export class ObjectStorage {
+    private _storage:Storage;
+
+    setStorage(storage:Storage) {
+        this._storage = storage;
+        return this;
+    }
+
+    getItem(key:string):any {
+        return JSON.parse(this._storage.getItem(key));
+    }
+
+    setItem(key:string, val:any) {
+        this._storage.setItem(key, JSON.stringify(val));
+        return this;
+    }
+}
