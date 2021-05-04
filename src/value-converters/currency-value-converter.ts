930eec7ce2fbd43d76956797dae54b7960fe6f9a
Mike Reiche <mike.reiche@t-systems.com>
2021-05-04 23:23:12 +0200

    Fixed precision calculation

--- src/value-converters/currency-value-converter.ts
+++ src/value-converters/currency-value-converter.ts
@@ -9,11 +9,16 @@ import {AbstractLocaleValueConverter} from "./abstract-locale-value-converter";
 @autoinject()
 export class CurrencyValueConverter extends AbstractLocaleValueConverter {
 
-    toView(value, currencyCode:string, precision:number=2): string {
+    toView(value:any, currencyCode:string, precision:number=2): string {
+        if (!value) {
+            return value;
+        }
+
         let realPrecision = precision;
-        while (value < 1/Math.pow(10, realPrecision)) {
+        while (value > 0 && value < 1/Math.pow(10, realPrecision)) {
             realPrecision += precision;
         }
+
         return new Intl.NumberFormat(CurrencyValueConverter.locale,{
             style: 'currency',
             currency: currencyCode,
