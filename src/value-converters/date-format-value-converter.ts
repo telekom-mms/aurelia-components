2902ac7b741f5281dcbd2024295140512c5be95a
Mike Reiche <rblm2e@gmail.com>
2022-04-18 21:35:32 +0200

    Refactored to instance methods

--- src/value-converters/date-format-value-converter.ts
+++ src/value-converters/date-format-value-converter.ts
@@ -8,24 +8,50 @@ import moment from "moment";
 export class DateFormatValueConverter {
     private static _defaultFormat = "LLL";
 
+    /**
+     * @deprecated Use instance method {@link setDefaultFormat} instead
+     */
     static setDefaultFormat(format:string) {
         this._defaultFormat = format;
     }
 
+
+    setDefaultFormat(format:string) {
+        DateFormatValueConverter._defaultFormat = format;
+    }
+
+    /**
+     * @deprecated Use instance method {@link getDefaultFormat} instead
+     */
     static getDefaultFormat() {
         return this._defaultFormat;
+    }
+
+    getDefaultFormat() {
+        return DateFormatValueConverter._defaultFormat;
     }
 
     toView(value, format:string = DateFormatValueConverter._defaultFormat): string {
         return DateFormatValueConverter.format(value, format);
     }
 
+    /**
+     * @deprecated Use {@link toView} instead
+     */
     static format(value: any, format: string = DateFormatValueConverter._defaultFormat): string {
         const moment = this.momentFromTimeValue(value);
         return moment.format(format);
     }
 
+    /**
+     * @deprecated Use instance method {@link momentFromTimeValue} instead
+     * @param value
+     */
     static momentFromTimeValue(value) {
+        return moment(value);
+    }
+
+    momentFromTimeValue(value:any) {
         return moment(value);
     }
 }
