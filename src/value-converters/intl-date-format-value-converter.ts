cc33955479bf730c3d1c325b7d8b4db586587994
Mike Reiche <mike@reiche.world>
2022-05-11 20:51:14 +0200

    Added value converter using IntlDateTimeFormat instead of momentjs.

---
+++ src/value-converters/intl-date-format-value-converter.ts
@@ -0,0 +1,23 @@
+import {valueConverter} from 'aurelia-binding'
+import {AbstractLocaleValueConverter} from "./abstract-locale-value-converter";
+
+/**
+ * Formats timestamps or strings to localized date formats
+ * Usage: ${dateProperty|dateFormat:"long"}
+ * @author Mike Reiche <mike@reiche.world>
+ */
+@valueConverter("dateFormat")
+export class IntlDateFormatValueConverter extends AbstractLocaleValueConverter {
+    private _options: {[key:string]: Intl.DateTimeFormatOptions } = {};
+
+    setOptions(id:string, options:Intl.DateTimeFormatOptions|object) {
+        this._options[id] = options;
+    }
+
+    toView(value:Date|string|number, optionId:string = null): string {
+        if (!(value instanceof Date)) {
+            value = new Date(value);
+        }
+        return Intl.DateTimeFormat(this.getLocale(), this._options[optionId]).format(value);
+    }
+}
---
+++ tests/value-converters/intl-date-format-value-converter.test.ts
@@ -0,0 +1,44 @@
+import 'aurelia-polyfills';
+import {Container} from "aurelia-dependency-injection";
+import {IntlDateFormatValueConverter} from "../../src/value-converters/intl-date-format-value-converter";
+
+const container = new Container();
+container.makeGlobal();
+
+const dateFormatter = container.get(IntlDateFormatValueConverter);
+dateFormatter.setLocale("en");
+dateFormatter.setOptions("full", { dateStyle: 'full', timeStyle: 'long' });
+
+const inputDate = new Date(0);
+const inputTimestamp = 0;
+const inputString = "1970-1-1 13:37:00"
+
+const formatData = [
+    {
+        input: inputDate,
+        optionId: null,
+        expectedFormat: '1/1/1970'
+    },
+    {
+        input: inputDate,
+        optionId: "full",
+        expectedFormat: 'Thursday, January 1, 1970 at 1:00:00 AM GMT+1'
+    },
+    {
+        input: inputTimestamp,
+        optionId: "full",
+        expectedFormat: 'Thursday, January 1, 1970 at 1:00:00 AM GMT+1'
+    },
+    {
+        input: inputString,
+        optionId: "full",
+        expectedFormat: 'Thursday, January 1, 1970 at 1:37:00 PM GMT+1'
+    }
+];
+
+describe.each(formatData)(`toView`, (data) => {
+    it(`dateFormats '${data.input}, option: ${data.optionId}'`, () => {
+        const formatted = dateFormatter.toView(data.input, data.optionId);
+        expect(formatted).toEqual(data.expectedFormat);
+    });
+});
