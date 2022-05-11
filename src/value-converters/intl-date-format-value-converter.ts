51e9441b94652c828c5ce3649ced8bfafc3472a1
Mike Reiche <mike@reiche.world>
2022-05-11 21:03:38 +0200

    Prevent error by setting default value.

--- src/value-converters/intl-date-format-value-converter.ts
+++ src/value-converters/intl-date-format-value-converter.ts
@@ -8,13 +8,13 @@ import {AbstractLocaleValueConverter} from "./abstract-locale-value-converter";
  */
 @valueConverter("dateFormat")
 export class IntlDateFormatValueConverter extends AbstractLocaleValueConverter {
-    private _options: {[key:string]: Intl.DateTimeFormatOptions } = {};
+    private _options: { [key: string]: Intl.DateTimeFormatOptions } = {};
 
-    setOptions(id:string, options:Intl.DateTimeFormatOptions|object) {
+    setOptions(id: string, options: Intl.DateTimeFormatOptions | object) {
         this._options[id] = options;
     }
 
-    toView(value:Date|string|number, optionId:string = null): string {
+    toView(value: Date | string | number = 0, optionId: string = null): string {
         if (!(value instanceof Date)) {
             value = new Date(value);
         }
