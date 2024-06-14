98aa8a1676b522130c18c189e6f3968c6e9e1cd9
Mike Reiche <mike.reiche@ewerk.com>
2024-06-14 19:10:21 +0200

    Fix test

--- src/value-converters/abstract-locale-value-converter.ts
+++ src/value-converters/abstract-locale-value-converter.ts
@@ -9,7 +9,6 @@ export class AbstractLocaleValueConverter {
     private readonly _eventAggregator = resolve(IEventAggregator)
 
     constructor() {
-        console.log("New Instance")
         this._eventAggregator.subscribe('i18n:locale:changed', (payload: { newValue: string; }) => {
             this.localeChanged(payload.newValue);
         });
