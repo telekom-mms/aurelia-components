b93ca9bc1cdf454b83c2757772c68c5980fd895d
Mike Reiche <mike.reiche@ewerk.com>
2024-06-14 19:26:41 +0200

    Add test for repeat-string-value-converter.ts

--- src/value-converters/repeat-string-value-converter.ts
+++ src/value-converters/repeat-string-value-converter.ts
@@ -1,7 +1,10 @@
+import {valueConverter} from "aurelia";
+
 /**
  * Repeats a value string n times
  * @author Mike Reiche <mike.reiche@t-systems.com>
  */
+@valueConverter("repeat")
 export class RepeatStringValueConverter {
     toView(
         value:string,
